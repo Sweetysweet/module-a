ffa55f1f4524c264d533ad4054385e1d82b5c98a
Сергей <s.gushchin@ifrbanking.ru>
2024-08-16 20:10:00 +0300

    add counter

---
+++ src/components/CounterComponent.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue'
+const counter = ref(0)
+
+const countHandler = () => {
+    counter.value++
+}
+</script>
+<template>
+<div>
+    <span>Счетчик: {{counter}}</span>
+    <button @click="countHandler">+1</button>
+</div>
+</template>
